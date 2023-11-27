27d47ed354f9864ba15863ff45a412c63492c45f
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-11-28 00:58:46 +0530

    set-up-complete

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
+app.get("/hello", (req, res) => {
+  res.send("welcome world");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
